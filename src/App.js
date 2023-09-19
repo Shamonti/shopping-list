aafc3e319015dc7e0d57b2e0356a79915844a6d1
Shamonti <67649277+Shamonti@users.noreply.github.com>
2023-09-19 12:38:07 +0600

    add and delete functionalities

--- src/App.js
+++ src/App.js
@@ -1,25 +1,106 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
-function App() {
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems(items => [...items, item]);
+  }
+
+  function handleToggleItem(id) {
+    setItems(
+      items.map(item =>
+        item.id === id ? { ...item, checked: !item.checked } : item
+      )
+    );
+  }
+
+  function handleDeleteItem(id) {
+    setItems(items => items.filter(item => item.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <Header />
+      <FormAddItems
+        onAddItems={handleAddItems}
+        onToggleItem={handleToggleItem}
+      />
+      <ItemsList
+        items={items}
+        onToggleItem={handleToggleItem}
+        onDeleteItem={handleDeleteItem}
+      />
     </div>
   );
 }
 
-export default App;
+function Header() {
+  return <h1>Shopping List</h1>;
+}
+
+function FormAddItems({ onAddItems }) {
+  const [item, setAddItem] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!item) return;
+
+    const id = crypto.randomUUID();
+
+    const newItem = {
+      id: id,
+      name: item,
+      checked: false,
+    };
+
+    onAddItems(newItem);
+    setAddItem('');
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Enter the items'
+        value={item}
+        onChange={e => setAddItem(e.target.value)}
+      />
+      <button className='button'>Add Items</button>
+    </form>
+  );
+}
+
+function ItemsList({ items, onToggleItem, onEditItem, onDeleteItem }) {
+  return (
+    <ul>
+      {items.map(item => (
+        <Item
+          item={item}
+          onToggleItem={onToggleItem}
+          onEditItem={onEditItem}
+          onDeleteItem={onDeleteItem}
+          key={item.id}
+        />
+      ))}
+    </ul>
+  );
+}
+
+function Item({ item, onToggleItem, onEditItem, onDeleteItem }) {
+  return (
+    <div>
+      <li>
+        <input type='checkbox' onChange={() => onToggleItem(item.id)} />
+        <p style={item.checked ? { textDecoration: 'line-through' } : null}>
+          {item.name}
+        </p>
+        <button className='button' onClick={onEditItem}>
+          Edit
+        </button>
+        <span onClick={() => onDeleteItem(item.id)}>❌</span>
+      </li>
+    </div>
+  );
+}
